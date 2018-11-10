3bc92a638b4903153a62aea5e4ca0d5387e70f6f
cairenhao1314 <512443601@qq.com>
2018-11-10 16:07:38 +0800

    第三次修改，再次测试！

--- src/com/game/Git.java
+++ src/com/game/Git.java
@@ -2,6 +2,7 @@ package com.game;
 
 public class Git {
     public static void main(String[] args) {
+        System.out.println(Test.getTestMethod());
         System.out.println("Hello Git,I come from IDEA!");
         System.out.println("--by 蔡仁浩");
     }
---
+++ src/com/game/Test.java
@@ -0,0 +1,7 @@
+package com.game;
+
+public class Test {
+    public static String getTestMethod(){
+        return "Hello Git ！";
+    }
+}
