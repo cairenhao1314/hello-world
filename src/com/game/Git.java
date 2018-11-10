4f2f2fce16997844e55a29f7485c5f7ba159e572
cairenhao1314 <512443601@qq.com>
2018-11-10 11:53:48 +0800

    第二次修改...

--- src/com/game/Git.java
+++ src/com/game/Git.java
@@ -3,5 +3,6 @@ package com.game;
 public class Git {
     public static void main(String[] args) {
         System.out.println("Hello Git,I come from IDEA!");
+        System.out.println("--by 蔡仁浩");
     }
 }
