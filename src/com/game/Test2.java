c7c1d167902a6c49a782f4b0ba0a717b49c4c21f
devops <>
2019-08-15 16:02:26 +0800

    我是ren

--- src/com/game/Test2.java
+++ src/com/game/Test2.java
@@ -4,6 +4,7 @@ public class Test {
     public static String getTestMethod(){
 	int a = 0;
         System.out.println("李四");
+        System.out.println("我是ren");
         return "Hello Git ！";
     }
 }
