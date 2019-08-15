bf06954a5da13daf8d83d946e039815b59a84015
devops <>
2019-08-15 15:42:36 +0800

    李四测试分支

--- src/com/game/Git.java
+++ src/com/game/Git.java
@@ -4,6 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println(Test.getTestMethod());
         System.out.println("Hello Git,I come from IDEA!");
-        System.out.println("--by 蔡仁浩");
+        System.out.println("--by 蔡仁浩dd");
+		
     }
 }
